db535363e88b5a7eaa1acdbbdac715429a9e8b0e
Arthur Gregorio <arthurshakal@gmail.com>
2021-08-22 20:14:01 -0300

    sem solid

--- solid/src/main/java/br/eti/arthurgregorio/isp/Isp.java
+++ solid/src/main/java/br/eti/arthurgregorio/isp/Isp.java
@@ -13,8 +13,10 @@ public class Isp {
 
         final var analista = new Analista("Jose", "123", 1000);
         final var vendedor = new Vendedor("Manuel", "321", 1500);
+        final var vendedor2 = new Vendedor("Juca", "999", 1700);
+        final var cozinheira = new Cozinheira("Maria", "456", 2500);
 
-        final List<Colaborador> colaboradores = List.of(analista, vendedor);
+        final List<Colaborador> colaboradores = List.of(analista, vendedor, cozinheira, vendedor2);
 
         colaboradores.forEach(colaborador -> System.out.printf("A comissão de %s é de %s",
                 colaborador.getNome(), colaborador.getComissao()).println());
